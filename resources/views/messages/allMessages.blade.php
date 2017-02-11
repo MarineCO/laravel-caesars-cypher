e00b71608ec9c28b1d573e4c1b0aece5233240e6
marineco <marinecolonge@gmail.com>
2017-02-11 19:04:51 +0100

    improvement of the homepage of messages

--- resources/views/messages/allMessages.blade.php
+++ resources/views/messages/allMessages.blade.php
@@ -3,36 +3,36 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Liste des messages</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.7/semantic.min.css">
 </head>
 <body>
-	<h1>Message list</h1>
+	<div class="ui centered grid">
+		<div class="four wide column">
+			<div class="ui center aligned container">
+				<h1>Message list</h1>
+			</div>
 
-	<table>
-		<tr>
-			<td>Id : </td>
-			<td>Content : </td>
-		</tr>
+			@foreach ($messages as $mess)
 
-		@foreach ($messages as $mess)
+			<div class="ui card">
+				<div class="ui center aligned segment">
+					<p>{{ $mess->id }} : {{ $mess->content }}</p>
+					<p>
+						<form action="/messages/delete/{{$mess->id}}" method="POST">{{csrf_field()}}
+							{{ method_field('DELETE') }}
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
+							<button class="ui red button">Delete</button>
+						</form>
+					</p>
+				</div>
+			</div>
 
-		<tr>
-			<td>{{ $mess->id }}</td>
-			<td>{{ $mess->content }}</td>
-			<td>
-				<form action="/messages/delete/{{$mess->id}}" method="POST">{{csrf_field()}}
-				{{ method_field('DELETE') }}
-				<input type="hidden" name="_token" value="{{ csrf_token() }}">
-				<button>Delete</button>
-				</form>
-			</td>
-		</tr>
-		
-		@endforeach
+			@endforeach
 
-	</table>
-
-	<form action="/messages/add">
-		<button>Encrypt a new message</button>
-	</form>
+			<form action="/messages/add">
+				<button class="big ui fluid green button">Encrypt a new message</button>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
