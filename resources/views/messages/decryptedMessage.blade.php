008473ffa157ad76b64e2c9855b10cca553d79d0
marineco <marinecolonge@gmail.com>
2017-02-12 20:04:43 +0100

    added a little of css

--- resources/views/messages/decryptedMessage.blade.php
+++ resources/views/messages/decryptedMessage.blade.php
@@ -3,11 +3,21 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Decrypted message</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/semantic-ui/2.2.7/semantic.min.css">
 </head>
 <body>
 	
 </body>
 </html>
-
-<h1>Decrypted message</h1>
-<h2>{{$decryptMess}}</h2>
+<div class="ui centered grid">
+	<div class="column">
+		<div class="ui center aligned container">
+			<div class="ui black inverted segment">
+				<h1>Decrypted message :</h1>
+			</div>
+			<div class="ui pink inverted segment">
+				<h2>{{$decryptMess}}</h2>
+			</div>
+		</div>
+	</div>
+</div>
